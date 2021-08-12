41346fd8fa8957d08100ff6587ea979cd91e5b96
François Voron <fvoron@gmail.com>
2021-08-12 17:21:18 +0200

    Implement footer and terms

--- website/components/Footer.tsx
+++ website/components/Footer.tsx
@@ -27,63 +27,48 @@ const Footer: React.FunctionComponent<FooterProps> = ({ }) => {
                 </a>
               </Link>
             </div>
-            <div className="text-sm text-gray-600">
-              <Link href="#" passHref><a className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Terms</a></Link> · <Link href="#"><a className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Privacy Policy</a></Link>
-            </div>
           </div>
 
           {/* 2nd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Products</h6>
+          <div className="lg:col-start-7 sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-400 font-medium mb-2">About me</h6>
             <ul className="text-sm">
               <li className="mb-2">
-                <Link href="#"><a className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Web Studio</a></Link>
+                <Link href="/references" passHref><a className="text-gray-500 hover:text-white transition duration-150 ease-in-out">References</a></Link>
+              </li>
+              <li className="mb-2">
+                <Link href="/fastapi-book" passHref><a className="text-gray-500 hover:text-white transition duration-150 ease-in-out">FastAPI book</a></Link>
+              </li>
+              <li className="mb-2">
+                <Link href="/blog" passHref><a className="text-gray-500 hover:text-white transition duration-150 ease-in-out">Blog</a></Link>
               </li>
             </ul>
           </div>
 
           {/* 3rd block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Resources</h6>
+            <h6 className="text-gray-400 font-medium mb-2">Open source</h6>
             <ul className="text-sm">
               <li className="mb-2">
-                <Link href="#"><a className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Web Studio</a></Link>
+                <a href="https://github.com/frankie567/fastapi-users" className="text-gray-500 hover:text-white transition duration-150 ease-in-out">FastAPI Users</a>
+              </li>
+              <li className="mb-2">
+                <a href="https://github.com/frankie567/httpx-oauth" className="text-gray-500 hover:text-white transition duration-150 ease-in-out">HTTPX OAuth</a>
+              </li>
+              <li className="mb-2">
+                <a href="https://github.com/frankie567/starlette-csrf" className="text-gray-500 hover:text-white transition duration-150 ease-in-out">Starlette CSRF Middleware</a>
               </li>
             </ul>
           </div>
 
           {/* 4th block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Company</h6>
+            <h6 className="text-gray-400 font-medium mb-2">Legal</h6>
             <ul className="text-sm">
               <li className="mb-2">
-                <Link href="#"><a className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Web Studio</a></Link>
+                <Link href="/terms" passHref><a className="text-gray-500 hover:text-white transition duration-150 ease-in-out">Terms</a></Link>
               </li>
             </ul>
-          </div>
-
-          {/* 5th block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
-            <h6 className="text-gray-800 font-medium mb-2">Subscribe</h6>
-            <p className="text-sm text-gray-600 mb-4">Get the latest news and articles to your inbox every month.</p>
-            <form>
-              <div className="flex flex-wrap mb-4">
-                <div className="w-full">
-                  <label className="block text-sm sr-only" htmlFor="newsletter">Email</label>
-                  <div className="relative flex items-center max-w-xs">
-                    <input id="newsletter" type="email" className="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm" placeholder="Your email" required />
-                    <button type="submit" className="absolute inset-0 left-auto" aria-label="Subscribe">
-                      <span className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300" aria-hidden="true"></span>
-                      <svg className="w-3 h-3 fill-current text-blue-600 mx-3 flex-shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                      </svg>
-                    </button>
-                  </div>
-                  {/* Success message */}
-                  {/* <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p> */}
-                </div>
-              </div>
-            </form>
           </div>
 
         </div>
@@ -94,30 +79,30 @@ const Footer: React.FunctionComponent<FooterProps> = ({ }) => {
           {/* Social links */}
           <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
             <li>
-              <a href="#" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
-                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
-                </svg>
-              </a>
-            </li>
-            <li className="ml-4">
-              <a href="#" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Github">
+              <a href="https://github.com/frankie567" className="flex justify-center items-center bg-gray-800 hover:bg-red-500 rounded-full shadow transition duration-150 ease-in-out" aria-label="Github">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                   <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
                 </svg>
               </a>
             </li>
             <li className="ml-4">
-              <a href="#" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Facebook">
+              <a href="https://twitter.com/fvoron" className="flex justify-center items-center bg-gray-800 hover:bg-red-500 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
+                  <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
+                </svg>
+              </a>
+            </li>
+            <li className="ml-4">
+              <a href="https://www.linkedin.com/in/fvoron/" className="flex justify-center items-center bg-gray-800 hover:bg-red-500 rounded-full shadow transition duration-150 ease-in-out" aria-label="LinkedIn">
+                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z"></path>
                 </svg>
               </a>
             </li>
           </ul>
 
           {/* Copyrights note */}
-          <div className="text-sm text-gray-500 mr-4">Made by <a className="text-blue-600 hover:underline" href="https://cruip.com/">Cruip</a>. All rights reserved.</div>
+          <div className="text-sm text-gray-500 mr-4">Made with ❤️ from 🇫🇷 - François Voron - SIRET 52101474600023</div>
 
         </div>
 
