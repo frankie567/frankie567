f7726204b4512560a78c9b28de4436325ad559bd
François Voron <fvoron@gmail.com>
2026-01-29 10:31:48 +0100

    Remove old Next.js website

--- website.old/image-loader.js
+++
@@ -1,8 +0,0 @@
-'use client';
-
-export default function dummyImageLoader({ src, width, quality }) {
-  if (src.startsWith('http')) {
-    return src;
-  }
-  return `${process.env.HOST}${src}?w=${width}&q=${quality || 75}`;
-}
